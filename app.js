339eab1f6069d375323052030dbe9f0157e83ff9
Kauã Xavier <kauaxavier032@gmail.com>
2023-12-04 21:54:19 -0300

    feat: multer config updated, user controller updated, user model updated, added dotenv and jsonwebtoken, added error handling

--- app.js
+++ app.js
@@ -3,11 +3,15 @@ import express from "express";
 import "dotenv/config";
 
 import connectDatabase from "./src/database/db.js";
+import router from "./src/routes/index.js";
 
 const app = express();
 connectDatabase();
 
 const port = process.env.PORT || 2000;
+
+app.use(express.json());
+app.use(router);
 
 app.listen(port, () => {
   console.log(`🟢 Server On ${port}`);
